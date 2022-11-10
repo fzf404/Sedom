fe5cfbf57272cb40bdde97aefa7970716a35e5ff
你们的小f <me@fzf404.art>
2022-11-10 15:58:05 +0800

    feat: 🎸 dark mode

--- js/index.js
+++ js/index.js
@@ -1,20 +1,64 @@
 import '../scss/index.scss'
 
-const a_color = [
-  'red',
-  'pink',
-  'purple',
-  'violet',
-  'indigo',
-  'blue',
-  'cyan',
-  'teal',
-  'green',
-  'lime',
-  'yellow',
-  'orange',
-]
+// 获取主题
+const getTheme = () => {
+  const theme = localStorage.getItem('theme') // 获取主题
+  return theme ? theme : 'dark'
+}
 
-document.querySelectorAll('.a-random').forEach((el) => {
-  el.setAttribute('class', `a-random a-${a_color[Math.floor(Math.random() * a_color.length)]}`)
-})
+// 切换主题
+const changeTheme = () => {
+  const theme = getTheme()
+  switch (theme) {
+    case 'dark':
+      document.documentElement.setAttribute('class', 'light')
+      localStorage.setItem('theme', 'light')
+      break
+    case 'light':
+      document.documentElement.setAttribute('class', 'dark')
+      localStorage.setItem('theme', 'dark')
+      break
+  }
+}
+
+// 初始化主题
+const initTheme = () => {
+  // 设置主题
+  const theme = getTheme()
+  document.documentElement.setAttribute('class', theme)
+  // 切换主题
+  document.querySelectorAll('.theme').forEach((el) => {
+    el.addEventListener('click', changeTheme)
+  })
+}
+
+// 初始化颜色
+const initColor = () => {
+  // 颜色表
+  const color = [
+    'red',
+    'pink',
+    'purple',
+    'violet',
+    'indigo',
+    'blue',
+    'cyan',
+    'teal',
+    'green',
+    'lime',
+    'yellow',
+    'orange',
+  ]
+  // 随机颜色
+  document.querySelectorAll('.a-rand').forEach((el) => {
+    el.setAttribute('class', `a-rand a-${color[Math.floor(Math.random() * color.length)]}`)
+  })
+}
+
+// 初始化 DOM
+const initDOM = () => {
+  initTheme()
+  initColor()
+}
+
+document.addEventListener('DOMContentLoaded', initDOM)
