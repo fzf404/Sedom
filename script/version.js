9304f7fd42e24cc60ce579daa02f25a35385f6da
你们的小f <me@fzf404.art>
2023-04-01 17:08:30 +0800

    fix: 🐛 sedom version

--- script/version.js
+++ script/version.js
@@ -1,8 +1,8 @@
-import pkg from '../package.json'
+import { version } from '../package.json'
 
 export const initVersion = () => {
   console.log(
-    `\n %c Sedom ${pkg.version} %c sedom.fzf404.art \n`,
+    `\n %c Sedom ${version} %c sedom.fzf404.art \n`,
     'color: #f1f3f5; background: #4263eb; padding:0.4rem 0;',
     'color: #212529; background: #dee2e6; padding:0.4rem 0;'
   )
