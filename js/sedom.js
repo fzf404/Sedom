a4c83109c37cfb4bd5876f9ca560f3afeac92c14
你们的小f <me@fzf404.art>
2023-04-04 09:28:29 +0800

    perf: ⚡️ remove random

--- js/sedom.js
+++ js/sedom.js
@@ -1,11 +1,9 @@
 import { initCode } from './code'
-import { initColor } from './color'
 import { initTheme } from './theme'
 import { initVersion } from './version'
 
 export const initDOM = () => {
   initTheme()
-  initColor()
   initCode()
   initVersion()
 }
