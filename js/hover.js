e5b4dc9314bc5de2fa76409383ff8cfc220b5371
你们的小f <me@fzf404.art>
2023-04-07 00:27:56 +0800

    feat: 🎸 hover event

---
+++ js/hover.js
@@ -0,0 +1,21 @@
+// 处理鼠标悬停事件
+export const initHover = () => {
+  document.querySelectorAll('abbr').forEach((element) => {
+    // 判断属性
+    if (element.getAttribute('title')) {
+      element.addEventListener('mouseenter', () => {
+        // 交换内容
+        const text = element.innerText
+        element.innerText = element.getAttribute('title')
+        element.setAttribute('title', text)
+      })
+      element.addEventListener('mouseleave', () => {
+        // 交换内容
+        const text = element.innerText
+        element.innerText = element.getAttribute('title')
+        element.setAttribute('title', text)
+      })
+
+    }
+  })
+}
--- js/sedom.js
+++ js/sedom.js
@@ -1,9 +1,11 @@
 import { initCode } from './code'
+import { initHover } from './hover'
 import { initTheme } from './theme'
 import { initVersion } from './version'
 
 export const initDOM = () => {
   initTheme()
   initCode()
+  initHover()
   initVersion()
 }
