6d0840772ab2c20d5f9cc109bb0d9b6d922c266f
fzf404 <me@fzf404.art>
2023-08-14 17:38:08 +0800

    perf: improve code generate

--- src/code.js
+++ src/code.js
@@ -1,33 +1,6 @@
-// 获取空格长度
-const getSpaceLength = (str) => {
-  let space = 0
-  for (let i = 0; i < str.length; i++) {
-    if (str[i] === ' ') {
-      space++
-    } else {
-      return space
-    }
-  }
-  return space
-}
-
 // 初始化代码
 export const initCode = () => {
-  document.querySelectorAll('code').forEach((element) => {
-    // 获取代码内容
-    const code = element.innerHTML
-      .replace(/^\s*\n/, '')
-      .replace(/\n\s*$/, '')
-      .split('\n')
-    // 获取代码缩进
-    const space = getSpaceLength(code[0])
-    // 清空代码内容
-    element.innerHTML = ''
-    // 重新添加代码内容
-    code.map((item) => {
-      const span = document.createElement('pre')
-      span.innerText = item.slice(space)
-      element.appendChild(span)
-    })
-  })
+  for (const element of document.querySelectorAll('code')) {
+    element.innerHTML = element.innerHTML.replaceAll(/\n\s*/g, '')
+  }
 }
