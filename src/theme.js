890ed4eca7dbce77b6b385fffd75479aa0347632
fzf404 <me@fzf404.art>
2023-08-14 18:08:49 +0800

    feat: judge system theme

--- src/theme.js
+++ src/theme.js
@@ -1,26 +1,35 @@
-// 获取主题
 export const getTheme = () => {
-  return localStorage.getItem('theme') || 'dark'
-}
-
-// 切换主题
-export const toggleTheme = () => {
-  switch (getTheme()) {
-    case 'dark':
-      document.documentElement.setAttribute('data-theme', 'light')
-      return localStorage.setItem('theme', 'light')
-    case 'light':
-      document.documentElement.setAttribute('data-theme', 'dark')
-      return localStorage.setItem('theme', 'dark')
+  switch (localStorage.getItem('SEDOM_THEME')) {
+    case 'dark': {
+      return 'dark'
+    }
+    case 'light': {
+      return 'light'
+    }
+    default: {
+      return window.matchMedia('(prefers-color-scheme: light)').matches
+        ? 'light'
+        : 'dark'
+    }
   }
 }
 
-// 初始化主题
+export const toggleTheme = () => {
+  switch (getTheme()) {
+    case 'dark': {
+      document.documentElement.dataset.theme = 'light'
+      return localStorage.setItem('SEDOM_THEME', 'light')
+    }
+    case 'light': {
+      document.documentElement.dataset.theme = 'dark'
+      return localStorage.setItem('SEDOM_THEME', 'dark')
+    }
+  }
+}
+
 export const initTheme = () => {
-  // 设置主题
-  document.documentElement.setAttribute('data-theme', getTheme())
-  // 切换主题
-  document.querySelectorAll('.theme').forEach((el) => {
+  document.documentElement.dataset.theme = getTheme()
+  for (const el of document.querySelectorAll('.theme')) {
     el.addEventListener('click', toggleTheme)
-  })
+  }
 }
