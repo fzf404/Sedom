e1b032ee7e6bb5d2f2870a242f0ed169929cb035
fzf404 <me@fzf404.art>
2024-04-27 16:32:24 +0800

    chore: update deps

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-import './styles/sedom.scss'
-
 import { init } from './scripts/sedom'
+
+import './styles/sedom.scss'
 
 export { init } from './scripts/sedom'
 export { getTheme, toggleTheme } from './scripts/theme'
